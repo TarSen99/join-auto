9e2ea5abb49af0dc2b8944db22fa8f145ebe73fb
Taras <senivtar1999@gmail.com>
2020-05-03 18:44:41 +0300

    changes

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,13 +1,13 @@
 define({
-  "name": "join-auto",
-  "version": "0.0.0",
-  "description": "",
+  "name": "Join auto API documentation",
+  "version": "0.1.0",
+  "description": "BASE_URL https://join-auto.herokuapp.com/v1",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-05-01T19:45:13.458Z",
+    "time": "2020-05-03T15:43:03.039Z",
     "url": "http://apidocjs.com",
     "version": "0.20.1"
   }
