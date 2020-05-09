3764db8ad600d3a06d9b64030b608fcdc7052d33
Taras <senivtar1999@gmail.com>
2020-05-09 18:00:00 +0300

    fixes

--- src/app.js
+++ src/app.js
@@ -30,8 +30,8 @@ app.options('*', function (req, res) {
 });
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
   next();
 });
 
