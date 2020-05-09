5b50db6c3f00fa662683a86a58b76ae8e9dced93
Taras <senivtar1999@gmail.com>
2020-05-09 17:29:38 +0300

    fixes

--- src/app.js
+++ src/app.js
@@ -23,6 +23,12 @@ var app = express();
 
 app.use(cors())
 app.options('*', cors());
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
