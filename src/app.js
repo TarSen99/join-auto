aad49424122f72cd029d5c07ec6113368a4ac709
Taras <senivtar1999@gmail.com>
2020-05-09 17:46:20 +0300

    fixes

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.options('*', cors());
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
