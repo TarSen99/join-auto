0f533df3069df05a9cf31fd35ed025d9a44ec04c
Taras <senivtar1999@gmail.com>
2020-06-08 21:52:16 +0300

    added ref token

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const AUTH_HEADER_NAME = 'Authorization'
 const MAX_PRICE_VALUE = Number.MAX_SAFE_INTEGER
 
-const USER_BASE_INFO_FIELDS = 'email user_name phone_number _id location created_at rating'
+const USER_BASE_INFO_FIELDS = 'email user_name phone_number _id location created_at rating referal_token'
 
 module.exports = {
   AUTH_HEADER_NAME,
